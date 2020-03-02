4b06bca473148b0c9a687490f78fe14cf227ec39
joshuakurien <32366300+joshuakurien@users.noreply.github.com>
2020-03-01 21:45:49 -0500

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -3,21 +3,19 @@ import time
 import random
 import json
 
-def publish(client, topic,qos):
-    payload = {1}
-    jsonpayload_sensor = json.dumps(payload, indent=4)
-    client.publish(topic, jsonpayload_sensor, qos=qos)
+def publish(client,topic):
+    client.publish(topic)
 
-def main(event):
+def main(event, context):
     message = event["queryStringParameters"]["topic"]
-    print("passed fron button click" + message)
-    # Connection parms for Solace Event broker
-    solace_url = "mr2j0vvhki1l0v.messaging.solace.cloud"
-    solace_port = 20358
+    
+    # Connection parameters for Solace Event broker
+    solace_url = "mr2ko4me0p6h2f.messaging.solace.cloud"
+    solace_port = 20134
     solace_user = "solace-cloud-client"
-    solace_passwd = "h9opao4l88b69t3nvqo2svv0b0"
+    solace_passwd = "u3ve6gpbjs62v5efj3uobk2564"
     solace_clientid = "lambda"
-   
+
     if message == 1:
         solace_topic = "openDoor"
     elif message == 2:
@@ -36,14 +34,15 @@ def main(event):
     # Instantiate/connect to mqtt client
     client = mqtt.Client(solace_clientid)
     client.username_pw_set(username=solace_user, password=solace_passwd)
-    print("Connecting to solace {}:{} as {}".format(solace_url, solace_port, solace_user))
     client.connect(solace_url, port=solace_port)
     client.loop_start()
 
-    # Publishes mesages to the MQTT bridge.
-    
-    publish(client, "light", solace_topic, 1)
+    # Publishes message to the MQTT bridge
+    publish(client, solace_topic)
  
+    client.loop_stop()
+    client.disconnect()
+
     return {
         'statusCode': 200,
         'body': "Success"
